f8563277403f8dd2565af08c8b1b207cab428a54
majinmaru <gotch11@naver.com>
2019-04-11 02:48:33 +0900

    190411
    goooo

--- home/javascripts/index.js
+++ home/javascripts/index.js
@@ -1,3 +1,78 @@
 $(document).ready(function(){
-	
+	var autoRun;
+	$(".main-slide").first().addClass('active');
+	$(".navigation").first().addClass('active');
+
+	$(".img-slide ").first().addClass('current');
+
+	$(".navigation").click(function(e){
+		e.preventDefault();
+		var getIndex = $(this).index();
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
+		$(".main-slide").eq(getIndex).addClass('active');
+		$(".main-slide").eq(getIndex).siblings().removeClass('active');
+	});
+	function autorunSlide(){
+		autoRun = setInterval(function() {
+			if($(".navigation.active").next().index() >= 0){
+				$(".navigation.active").next().click();
+			} else {
+				$(".navigation").first().click();
+			}
+        }, 5000);    
+	}
+	autorunSlide();
+
+	function resetAutorun(){
+		$(".navigation").click(function(e){
+			e.preventDefault();
+			clearInterval(autoRun);
+			autorunSlide();
+		});
+	}
+	resetAutorun();
+
+	$(".box-img").hover(function(){
+		$(this).find(".img-slide__img img").addClass('active');
+		$(this).find(".img-slide__info").css('opacity', '1');
+	}, function(){
+		$(this).find(".img-slide__img img").removeClass('active');
+		$(this).find(".img-slide__info").css('opacity', '0');
+	});
+
+	$(".arrow").click(function(e){
+		e.preventDefault();
+		var currentSlide = $(".img-slide").filter('.current');
+		var getIndex = currentSlide.index();
+		if($(this).hasClass('prev')){
+			if(currentSlide.prev().index() >= 0){
+				$(".img-slide").eq(getIndex).removeClass('current');
+				$(".img-slide").eq(getIndex).prev().addClass('current');
+			} else {
+				$(".img-slide").eq(getIndex).removeClass('current');
+				$(".img-slide").last().addClass('current');
+			}
+		} else if ($(this).hasClass('next')) {
+			if(currentSlide.next().index() >= 0){
+				$(".img-slide").eq(getIndex).removeClass('current');
+				$(".img-slide").eq(getIndex).next().addClass('current');
+			} else {
+				$(".img-slide").eq(getIndex).removeClass('current');
+				$(".img-slide").first().addClass('current');
+			}
+		}
+	});
+
+	$(".winner-cheer").paroller({
+		factor: 0.5,
+		type: 'foreground',
+		direction: 'horizontal',
+		transition: 'transform 0.5s linear'
+	});
+
+	$(".room-pick-wrapper").mCustomScrollbar({
+		axis:"x",
+	    theme: "minimal-dark"
+	});
 });
