c2188318f4c20b254edfe89820008423b4309f24
majinmaru <gotch11@naver.com>
2019-04-18 16:32:06 +0900

    190418
    PC끝

--- furniture/javascripts/index.js
+++ furniture/javascripts/index.js
@@ -4,4 +4,35 @@ $(document).ready(function(){
 		$(this).addClass('active');
 		$(this).siblings().removeClass('active');
 	});
+
+	$(".room-arrow").click(function(e){
+		var currentRoom = $(this).parents(".all-room");
+		var currentRoomImg = currentRoom.find(".room-graphic").filter('.active');
+		var getIndex = currentRoomImg.index();
+		if($(this).hasClass('prev')){
+			if(currentRoomImg.prev().index() >= 0){
+				currentRoom.find(".room-graphic").eq(getIndex).removeClass('active');
+				currentRoom.find(".room-graphic").eq(getIndex).prev().addClass('active');
+				currentRoom.find(".room-content").eq(getIndex).removeClass('current');
+				currentRoom.find(".room-content").eq(getIndex).prev().addClass('current');
+			} else {
+				currentRoom.find(".room-graphic").eq(getIndex).removeClass('active');
+				currentRoom.find(".room-graphic").last().addClass('active');
+				currentRoom.find(".room-content").eq(getIndex).removeClass('current');
+				currentRoom.find(".room-content").last().addClass('current');
+			}
+		} else if ($(this).hasClass('next')) {
+			if(currentRoomImg.next().index() >= 0){
+				currentRoom.find(".room-graphic").eq(getIndex).removeClass('active');
+				currentRoom.find(".room-graphic").eq(getIndex).next().addClass('active');
+				currentRoom.find(".room-content").eq(getIndex).removeClass('current');
+				currentRoom.find(".room-content").eq(getIndex).next().addClass('current');
+			} else {
+				currentRoom.find(".room-graphic").eq(getIndex).removeClass('active');
+				currentRoom.find(".room-graphic").first().addClass('active');
+				currentRoom.find(".room-content").eq(getIndex).removeClass('current');
+				currentRoom.find(".room-content").first().addClass('current');
+			}
+		}
+	});
 });
